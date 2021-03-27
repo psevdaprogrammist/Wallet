3e1c34a626e6f12dccd2ca175fa42740cb999fdd
Egor <korchagin.egorka1@gmail.com>
2021-03-27 13:33:21 +1000

    Initial Commit

---
+++ Wallet/WalletApp.swift
@@ -0,0 +1,17 @@
+//
+//  WalletApp.swift
+//  Wallet
+//
+//  Created by Egor Korchagin on 27.03.2021.
+//
+
+import SwiftUI
+
+@main
+struct WalletApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
